67195d4411550411279f2440a5bc9d832e5196e6
codingsh <developerfred@protonmail.ch>
2020-04-25 16:33:24 -0300

    test: add json call for test structs #32 - add client file

--- go.mod
+++ go.mod
@@ -7,5 +7,4 @@ require (
 	github.com/jedib0t/go-pretty v4.3.0+incompatible
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/spf13/cobra v0.0.6
-	gotest.tools/gotestsum v0.4.2 // indirect
 )
--- library/structs_test.go
+++ library/structs_test.go
@@ -1,23 +1,16 @@
 package library
 
 import (
-	"encoding/json"
 	"testing"
-	// "fmt"
 )
 
 func TestEventStrcuts(t *testing.T) {
+	urlEvents := "https://eonet.sci.gsfc.nasa.gov/api/v3/events"
+	event := new(EventResponse)
 
-	inputJSON := "{\n\t\"id\": \"EONET_4637\",\n\t\"title\": \"Wildfire - Vichuquen, Chile\",\n\t\"description\": null,\n\t\"link\": \"https://eonet.sci.gsfc.nasa.gov/api/v3/events/EONET_4637\",\n\t\"closed\": null,\n\t\t\"categories\": [\n\t\t\t{\n\t\t\t\"id\": \"wildfires\",\n\t\t\t\"title\": \"Wildfires\"\n\t\t}\n\n\t\t\t\t\t\t],\n\t\t\"sources\": [\n\t\t\t{\n\t\t\t\"id\": \"PDC\",\n\t\t\t\"url\": \"http://emops.pdc.org/emops/?hazard_id=107743\"\n\t\t}\n\n\t\t\t\t\t\t\n\t],\n\t\t\"geometry\": [\n\t\t\t{\n\t\t\t\t\t\"magnitudeValue\": null,\n\t\t\t\"magnitudeUnit\": null,\n\t\t\t\"date\": \"2020-04-22T16:59:00Z\",\n\t\t\t\"type\": \"Point\", \n\t\t\t\t\t\"coordinates\": [ -72.02108, -34.84119 ]\n\t\t\t\t\t}\n\n\t\t\t\t]\n}"
+	getJson(urlEvents, event)
 
-	var response EventResponse
-
-	err := json.Unmarshal([]byte(inputJSON), &response)
-	if err != nil {
-		t.Errorf("An error occured during unmarshal: %v\n Wanted: %v", response, "Pass")
-	}
-
-	for _, value := range response.Events {
+	for _, value := range event.Events {
 		got := value.Title
 		want := "Wildfire - Vichuquen, Chile"
 		if got != want {
@@ -29,16 +22,12 @@ func TestEventStrcuts(t *testing.T) {
 
 func TestCategoriestrcuts(t *testing.T) {
 
-	inputJSON := "{\n\t\"title\": \"EONET Events: Drought\",\n\t\"description\": \"Long lasting absence of precipitation affecting agriculture and livestock, and the overall availability of food and water.\",\n\t\"link\": \"https://eonet.sci.gsfc.nasa.gov/api/v3/categories/drought\",\n\t\"events\": [\n\t]\n}"
+	urlCategories := "https://eonet.sci.gsfc.nasa.gov/api/v3/categories/"
+	CategoriesRes := new(CategoriesResponse)
 
-	var response CategoriesResponse
+	getJson(urlCategories, CategoriesRes)
 
-	err := json.Unmarshal([]byte(inputJSON), &response)
-	if err != nil {
-		t.Errorf("An error occured during unmarshal: %v\n Wanted: %v", response, "Pass")
-	}
-
-	for _, value := range response.Categories {
+	for _, value := range CategoriesRes.categories {
 		got := value.Title
 		want := "EONET Events: Drought"
 		if got != want {
