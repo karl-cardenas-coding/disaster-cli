34fcaf5c14e80b0592cfa0955db405a939cdd605
cardenas88karl@gmail.com <cardenas88karl@gmail.com>
2020-04-25 12:25:21 -0700

    ci: changing semantic-release config

--- release.config.js
+++ release.config.js
@@ -3,6 +3,10 @@ module.exports = {
   repositoryUrl: "https://github.com/karl-cardenas-coding/disaster-cli.git",
   plugins: ["@semantic-release/commit-analyzer",
    "@semantic-release/release-notes-generator",
+   ["@semantic-release/exec", {
+    "analyzeCommitsCmd": "echo 'NEW_VERSION=false' > VERSION.env",
+    "verifyReleaseCmd": "echo 'export VERSION=${nextRelease.version}\nNEW_VERSION=true' > VERSION.env"
+  }],
    ["@semantic-release/github", {
       "assets": ["*.zip"]
       }
