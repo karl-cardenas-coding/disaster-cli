0dcedc661a73e84306e825f49b866eb30d555c34
cardenas88karl@gmail.com <cardenas88karl@gmail.com>
2020-04-24 20:48:46 -0700

    ci: fixed yaml syntax error-6

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "master",
-  repositoryUrl: "https://github.com/karl-cardenas-coding/disaster-cli",
+  repositoryUrl: "https://github.com/karl-cardenas-coding/disaster-cli.git",
   plugins: ["@semantic-release/commit-analyzer",
    "@semantic-release/release-notes-generator",
    ["@semantic-release/github", {
