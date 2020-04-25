3e099e555dedc7bfb84e4efd75175301d1528597
cardenas88karl@gmail.com <cardenas88karl@gmail.com>
2020-04-25 12:02:28 -0700

    ci: adding version-4

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: "master",
+  branches: ["master", "clean-up"],
   repositoryUrl: "https://github.com/karl-cardenas-coding/disaster-cli.git",
   plugins: ["@semantic-release/commit-analyzer",
    "@semantic-release/release-notes-generator",
